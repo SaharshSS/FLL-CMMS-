d5ad69cb45bfa9da0a84772c48cb531c81a1da96
SaharshSS <67810603+SaharshSS@users.noreply.github.com>
2023-12-04 16:12:01 -0800

    Create AccuracyTest.py

---
+++ AccuracyTest.py
@@ -0,0 +1,21 @@
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+
+# Initialize both motors. In this example, the motor on the
+# left must turn counterclockwise to make the robot go forward.
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.B)
+
+# Initialize the drive base. In this example, the wheel diameter is 56mm.
+# The distance between the two wheel-ground contact points is 80mm.
+# If you would like to convert studs to mm, go here: http://studs.sariel.pl/
+drive_base = DriveBase(left_motor, right_motor, wheel_diameter=89.231, axle_track=100)
+
+# Optionally, uncomment the line below to use the gyro for improved accuracy.
+drive_base.use_gyro(True)
+
+
+
+drive_base.straight(1000)
+
