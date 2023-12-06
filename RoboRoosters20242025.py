e40b9edf187cf86dce7b4f82d391e8e5a64bd7af
KK <151797581+k329272@users.noreply.github.com>
2023-12-06 09:27:36 -0800

    Add Robo Roosters 2024 2025 code
    Code for FLL next year

---
+++ RoboRoosters20242025.py
@@ -0,0 +1,87 @@
+from pybricks import version
+from pybricks.parameters import Color, Button, Port, Direction, Motor, UltrasonicSensor
+from pybricks.robotics import DriveBase
+from pybricks.hubs import PrimeHub
+from pybricks.tools import multitask, run_task, wait
+Commands = [
+    "Done"
+]
+Status = "Starting"
+def Setup():
+    hub.light.animate([Color.RED * (0.5 * sin(i / 15 * pi) + 0.5) for i in range(30)], 40)
+    hub.system.set_stop_button(Button.BLUETOOTH)
+    status="Loading"
+    print(usys.implementation)
+    print(usys.version)
+    system.name("Rooster")
+    if (system.reset_reason() == 2):
+        print("Rebooting from error")
+    print("Booting")
+    print("Battery voltage: " + str(battery.voltage()) + " mV")
+    print("Battery current: " + str(battery.current()) + " mA")
+    if charger.connected():
+        if (charger.status() == 1):
+            print("Charging")
+        else if (charger.status() == 2):
+            print("Battery full")
+        else:
+            print("Error!")
+    Dist = UltrasonicSensor(Port.E)
+    lights.on(1, 1, 0, 0)
+    ArmA = Motor(Port.D)
+    ArmB = Motor(Port.F)
+    ColorSense = ColorSensor(Port.C)
+    ColorSense.lights.on([100, 0, 0])
+    left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+    right_motor = Motor(Port.B)
+    drive = DriveBase(left_motor, right_motor, wheel_diameter=89.231, axle_track=100)
+    drive.use_gyro(True)
+    light.blink(color.red, 0.5)
+def turnArm(Arm1, Arm2, Mode = 2, Speed = 500):
+    if (Mode = 2):
+        multitask(ArmA.run_target(Speed, Arm1, then = Stop.HOLD), ArmB.run_target(Speed, Arm2, then = Stop.HOLD))
+    if (Mode = 1):
+        ArmA.run_target(Speed, Arm1, then = Stop.HOLD)
+        ArmB.run_target(Speed, Arm2, then = Stop.HOLD)
+    else:
+        ArmB.run_target(Speed, Arm2, then = Stop.HOLD)
+        ArmA.run_target(Speed, Arm1, then = Stop.HOLD)
+def TrackLine(Distance):
+    NewDirection=0
+    NewDistance=0
+    while (Distance > NewDistance):
+        if (ColorSense.color() == "White"):
+            if (NewDirection = 1):
+                NewDirection = 0
+                while (ColorSense.color() != "White"):
+                    drive.curve(20, 1, Stop.COAST)
+        else:
+            NewDirection = 1
+                    if (ColorSense.color() == "White"):
+            if (NewDirection = 1):
+                NewDirection = 0
+                while (ColorSense.color() != "White"):
+                    drive.curve(-20, 1, Stop.COAST)
+        drive.straight(1)
+        NewDistance=NewDistance+0.5
+def TrackLine1(Distance, Direction = True):
+    NewDirection = 0
+    NewDistance = 0
+    while (Distance>NewDistance):
+        #Code later Kevin annoying
+def TestAll():
+    TestVar = int(input("Testing serial connection, please press one"))
+    if (TestVar == 1):
+        print("Success")
+    else:
+        print("Error")
+    turnArm(0, 0, 2, 1000)
+    if (ArmA.angle() == 0 && ArmB.angle() == 0):
+        print("Success")
+    else:
+        print("Error")
+def Move(count = 0):
+    actionCount=0
+    while (status != "Done"):
+        count = count + 2
+        movement = Commands[count-1]
