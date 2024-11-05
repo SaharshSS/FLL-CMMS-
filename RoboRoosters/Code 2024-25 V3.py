4e97dd80bd92b059e0d00169a4a283858a3004ae
KK122 <k329272@gmail.com>
2024-11-04 16:16:25 -0800

    Update Code 2024-25 V3.py
    Work day

--- RoboRoosters/Code 2024-25 V3.py
+++ RoboRoosters/Code 2024-25 V3.py
@@ -7,7 +7,7 @@ from pybricks import version
 
 hub = PrimeHub()
 
-armMid = Motor(Port.D)
+armMid = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 armBase = Motor(Port.F, Direction.COUNTERCLOCKWISE)
 
 left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
@@ -23,13 +23,11 @@ def setup():
     drive.reset()
     print("Version " + str(version))
     drive.use_gyro(True)
-    #drive.settings(straight_speed = 7500, straight_acceleration = 1000, turn_rate = 500, turn_acceleration = 100)
+    drive.settings(straight_speed = 500, straight_acceleration = 1000, turn_rate = 500, turn_acceleration = 100)
     hub.imu.reset_heading(0)
     resetArm()
 
 def turnArm(angle1, angle2, mode = 0, speed = 500):
-    angle1 = constrain(angle1, 85, -25)
-    angle2 = constrain(angle2, 90, 0)
     if not mode:
         armMid.run_target(speed, angle1, wait = False)
         armBase.run_target(speed, angle2)
@@ -49,7 +47,7 @@ def across():
     drive.straight(1000)
 
 def resetArm():
-    turnArm(85, 0, 3, 100)
+    turnArm(0, 90, 3, 100)
     
 def circle():
     drive.curve(300, 360)
@@ -61,13 +59,48 @@ def task1():
     drive.turn(-45)
     drive.straight(200*3)
     drive.turn(-45)
-    drive.straight(100)
-    wait(100)
-    drive.straight(-100)
+    drive.straight(150)
+    wait(1000)
+    drive.straight(-150)
     drive.turn(45)
     drive.straight(200*-3)
     drive.turn(45)
     drive.straight(288*-1.5)
+
+def task2():
+    drive.turn(45)
+    drive.straight(200)
+    drive.turn(-45)
+    drive.straight(500)
+    drive.turn(-45)
+    drive.straight(20)
+    turnArm(0, 0)
+    wait(1000)
+    resetArm()
+    drive.straight(-20)
+    drive.turn(45)
+    drive.straight(-500)
+    drive.turn(45)
+    drive.straight(-200)
+
+def task3():
+    pass
+
+def task4():
+    pass
+
+def task5():
+    pass
+
+def task6():
+    drive.straight(500)
+
+def task7():
+    turnArm(0, 0, mode = 4)
+    drive.turn(-45)
+    drive.straight(500)
+    drive.straight(-500)
+    resetArm()
 
 def task8():
     turnArm(85, 90)
@@ -100,12 +133,24 @@ def task10():
     drive.turn(-45)
     drive.straight(-200)
 
+def task11():
+    pass
+
+def task12():
+    pass
+
+def task13():
+    pass
+
+def task14():
+    pass
+
 def task15():
     resetArm()
     drive.straight(800)
     drive.straight(-200)
 
-tasks = [across, resetArm, circle, task1, task8, task9, task10, task15]
+tasks = [task1, task2, task3, task4, task5, task6, task7, task8, task9, task10, task11, task12, task13, task14, task15]
 inputs = []
 inputCount =  0
 menuindex = 0
@@ -168,7 +213,10 @@ def main():
                 dispOn = False
                 hub.display.off()
             else:
-                hub.display.char(str(menuindex))
+                if menuindex >= 10:
+                    hub.display.number(menuindex)
+                else: 
+                    hub.display.char(str(menuindex))
                 dispOn = True
             wait(100)
         try:
