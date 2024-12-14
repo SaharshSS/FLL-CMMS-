89875e547b850ea11ba57c1077384ceaf0a5253f
KK122 <k329272@gmail.com>
2024-12-14 11:28:00 -0800

    Add map function

--- RoboRoosters/v4/Math.py
+++ RoboRoosters/v4/Math.py
@@ -6,3 +6,6 @@ def constrain(value, minimum, maximum):
     if value > maximum:
         return maximum
     return value
+
+def reMap(value, in_min, in_max, out_min, out_max):
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
