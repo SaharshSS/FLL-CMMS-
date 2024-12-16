7b2410eff4c624034c0175f753262e0faddb7f57
KK122 <k329272@gmail.com>
2024-12-15 19:00:48 -0800

    Update and rename color.py to Color.py

--- RoboRoosters/v4/Color.py
+++ RoboRoosters/v4/Color.py
@@ -2,3 +2,7 @@ from pybricks.pupdevices import ColorSensor
 from pybricks.parameters import Port, Color
 color = ColorSensor(port=Port.C)
 color.lights.off()
+def getColor():
+    return color.color()
+def getBrightness():
+    return color.ambient()
