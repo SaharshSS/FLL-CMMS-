0e873d2acf7aa324ead51d6f04193120cafa23de
KK122 <k329272@gmail.com>
2024-12-28 10:56:49 -0800

    Create Timer.py

---
+++ RoboRoosters/v5/Timer.py
@@ -0,0 +1,11 @@
+from pybricks.tools import StopWatch
+watch = StopWatch()
+watch2 = StopWatch()
+def reset():
+    watch.reset()
+def time():
+    return watch.time()
+def startMission():
+    watch2.reset()
+def mTime():
+    return watch2.time()
