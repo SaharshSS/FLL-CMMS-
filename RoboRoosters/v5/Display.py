ff9865f582f6673529f535efeebe7babecce9dca
KK122 <k329272@gmail.com>
2025-01-06 21:09:31 -0800

    Recoded it to finally work
    I was definitely half asleep while writing the prior one

--- RoboRoosters/v5/Display.py
+++ RoboRoosters/v5/Display.py
@@ -1,18 +1,21 @@
 from pybricks.parameters import Icon
 from pybricks.hubs import PrimeHub
-hub = PrimeHub
-hub.display.icon(Icon.FALSE)
-displayOn = False
 
-def display(displayVal, displayForceOn = False):
-    if displayForceOn == True:
-        displayOn = False
-    if displayOn = True:
+hub = PrimeHub()
+hub.display.icon(Icon.FALSE)
+
+def display(displayVal, displayOn = False):
+    if displayOn == True:
         hub.display.off()
-        break
-    if isinstance(displayVal, int):
-        if displayVal > 9:
-            hub.display.number(displayVal)
-            break
-        displayVal = str(displayVal)
-    hub.display.char(displayVal)
+        displayOn = False
+    else:
+        displayOn = True
+        if isinstance(displayVal, int):
+            if displayVal < 10:
+                hub.display.number(displayVal)
+            else:
+                displayVal = str(displayVal)
+                hub.display.char(displayVal)
+        else:
+            hub.display.char(displayVal)
+    return displayOn
