3681dfd7878a7c3f529adca4063571f24b7837a6
KK122 <k329272@gmail.com>
2025-01-23 13:43:20 -0800

    Update Missions.py

--- RoboRoosters/v5/Missions.py
+++ RoboRoosters/v5/Missions.py
@@ -1,72 +1,79 @@
 from pybricks.tools import wait
+from pybricks.parameters import Stop
 import Arm, Drive
 
 def mission1():
-    Arm.turnArm(52.5, 52.5)
-    wait(500)
-    Drive.straight(200)
+    Arm.turn(52.5, 52.5, mode = 3)
+    wait(100)
+    Drive.straight(300, then=Stop.COAST_SMART)
     Drive.back(150)
-    Arm.resetArm()
+    Arm.reset()
 
 def mission2():
     Drive.turn(-10)
-    Arm.turnArm(0, 0)
+    Arm.hook()
     wait(500)
-    Arm.resetArm()
-    Arm.armUp()
+    Arm.reset()
+    Arm.up()
 
 def mission3():
-    Drive.turn(90)
-    Arm.resetArm()
-    Drive.straight(50)
-    Arm.turnArm(0, 0, 2, 2500)
-    Drive.turn(-20)
-    Arm.resetArm()
-    Arm.armUp()
-    Drive.turn(110)
+    Drive.turn(90, wait=False)
+    Arm.reset()
+    Drive.straight(50, wait=False)
+    wait(200)
+    Arm.turn(25, -10, 2, 2500)
+    Drive.turn(-20, wait=False)
+    Arm.reset()
+    Drive.turn(110, wait=False)
+    Arm.up()
 
 def mission4():
     pass
 
-def mission5():    
+def mission5():
     Arm.hook()
     Drive.back(75)
-    Drive.turn(30)
-    Arm.resetArm()
-    Drive.turn(-75)
+    Drive.turn(25)
+    Arm.reset()
+    Drive.turn(-70)
 
 
 def mission6():
-    Arm.turnArm(10, 0)
-    Drive.straight(175)
-    Arm.turnArm(45, 45)
-    Arm.disableArm()
-    Drive.straight(50)
-    Drive.turn(-65, speed=100)
+    Arm.turn(10, 0)
+    Drive.straight(175, then=Stop.COAST_SMART)
+    Drive.back(25, wait=False)
+    Arm.turn(45, 45, mode=3)
+    Drive.straight(50, speed=50)
+    wait(500)
+    Drive.back(25)
+    Drive.turn(-50)
+    Arm.disable()
     Drive.back(100)
-    Drive.turn(20)
+    Drive.turn(5)
 
 def mission7():
     pass
 
 def mission8():
-    Arm.turnArm(0, 25)
-    Drive.back(200)
-    Arm.resetArm()
+    Arm.hook()
+    Arm.disable()
+    Drive.back(150)
+    Arm.up()
     for i in range(4):
-        Arm.armUp()
-        Drive.straight(200)
-        Arm.turnArm(20, 90)
+        Arm.up()
+        Drive.straight(150)
+        Arm.turn(20, 90)
         Drive.back(300)
 
+
 def mission9():
-    Arm.armUp()
-    Drive.straight(500)
+    Drive.straight(500, wait=False)
+    Arm.up()
     Drive.back(300)
 
 def mission10():
-    Drive.turn(-15)
-    Arm.turnArm(70, 70, 1, 2500)
+    Drive.turn(-15, wait = False)
+    Arm.turn(65, 65, 0, 2500)
     wait(1500)
     Drive.turn(15)
 
@@ -78,25 +85,30 @@ def mission12():
     Drive.back(200)
 
 def mission13():
-    Drive.back(100)
-    Arm.turnArm(0, 10)
-    wait(50)
-    Arm.disableArm()
+    Drive.back(150, wait=False)
+    Arm.turn(5, 0, mode = 2)
+    Arm.disable()
     Drive.straight(300, speed=100)
-    Arm.resetArm()
-    Arm.armUp()
+    Arm.reset()
+    Arm.up()
     Drive.turn(-90)
-    Drive.back(50)
+    Drive.back(25, wait=False)
     Arm.hook()
-    Drive.turn(-45, speed=250)
+    Drive.turn(45, speed=500)
     Drive.turn(45)
 
 def mission14():
-    pass
+    Drive.back(100, wait=False)
+    Arm.turn(20, 20)
+    Drive.straight(150)
+    Arm.turn(30, 95, 1, 500)
+    Drive.front_square()
+    Drive.back(50)
 
 def mission15():
     Drive.straight(500)
     Drive.back(300)
     Drive.turn(-90)
     Drive.back_square()
-    
+
+print("Missions.py loaded")
